9ac67e19e67316d9059e9ee7ba58c9b473dd40a7
sanketana <73871934+sanketana@users.noreply.github.com>
2022-03-16 23:43:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
